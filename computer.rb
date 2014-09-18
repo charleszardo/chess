05702a24f28a7130d8ac0f1efa8916b72d9fdd94
John Zhang <john.shimin.zhang@gmail.com>
2014-09-17 20:33:43 -0400

    add evaluate method

--- computer.rb
+++ computer.rb
@@ -1,7 +1,31 @@
+#TO DO:
+
+# Minimax
+
+# AB Pruning
+
+# Iterative Deepening
+
+# Transposition Table??
+
+require_relative "board"
+require_relative "pieces"
+require_relative "cursor"
+require 'colorize'
+
 class ComputerPlayer
-  attr_reader :board
-  def initialize(board, color)
-    @board = board
+  OPP = {white: :black,
+       black: :white}
+  VALUES = {
+    "Queen" => 900,
+    "Rook" => 500,
+    "Knight" => 300, 
+    "Bishop" => 325, 
+    "Pawn" => 100,
+    "King" => 0
+  }
+  attr_reader :color
+  def initialize(color)
     @color = color
   end
 
@@ -18,7 +42,9 @@ class ComputerPlayer
     #go through every possible moves of players units
     #dup board, gets N board. Each board has value of V
     #if my color is their color, choose one which gives max board position
-    return max(boards.map{|board| step(board, player, turns - 1)})
+    return max(boards.map{|board| step(board, player, turns - 1)}) if player.color = self.color
+    #else return min of all boards
+    return max(boards.map{|board| step(board, player, turns - 1)}) unless player.color = self.color
   end
 
   def move
@@ -34,9 +60,28 @@ class ComputerPlayer
   end
 
   def evaluate(board)
-    return -inifinity if board.checkmate(self.color)
-    board.army(:black).inject(values hash blah blah) + board.army(:white)
+    my_value = self.player_score(board, self.color)
+    opp_value = self.player_score(board, OPP[self.color])
+    my_value - opp_value
+  end
+  
+  def player_score(board, color)
+    return -20000 if board.checkmate?(color)
+    army = board.army(color)
+    army.inject(0) do |acc,unit|
+       acc + VALUES[unit.class.to_s]
+     end
   end
 
+end
 
+if __FILE__ == $PROGRAM_NAME
+  board = Board.new
+  player = ComputerPlayer.new(:white)
+  board.render(:white)
+  p player.evaluate(board)
+  board[[0,1]] = nil
+  p player.evaluate(board)
+  
+  
 end

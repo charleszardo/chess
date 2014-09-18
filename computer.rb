c298d88f14af8d10500617c30b20e33e97f148e8
John Zhang <john.shimin.zhang@gmail.com>
2014-09-17 23:40:12 -0400

    working on minimax

--- computer.rb
+++ computer.rb
@@ -42,15 +42,40 @@ class ComputerPlayer
     crystal_ball.each do |time_shard|
       step_state = self.moved_state(board, time_shard.first)
       time_shard << step_state
+      time_shard << evaluate(step_state)
       next_color = OPP[color]
       recurse = future_tree(step_state, future_level, next_color)
       time_shard << recurse
     end
-    
+    #future tree has the structure of [move, board, value, future_tree(lvl -1)]
     crystal_ball
   end
   
-  def min_max
+  def min_max(board)
+    future_tree = future_tree(board, 0)
+    
+    #assuming we are at bottom
+    # max = future_tree.map{|node| node[2]}.max
+ #    puts max
+ #p future_tree.first.last.first#
+ # puts future_tree.first.first #whites's this is correct
+ #
+ # puts future_tree.first.last.first.first #black's turn correct
+ #
+ # puts future_tree.first.last.first.last.first.first #white's turn again, not right
+ #
+ # puts future_tree.first.last.first.last.first.last #Final Black Value 2 levels why
+ p future_tree
+   p self.max_of_nodes(future_tree)
+    #bottom of the tree
+    
+    
+    #one level up (player color to maximize)
+    # min = future_tree.map{|node| node.last}.min
+    # p min
+    # future_tree.select do |node|
+    #   node.last = future_tree
+    
     # We Want To Maximize The Minimum Opponent Player Score
 
     
@@ -58,8 +83,23 @@ class ComputerPlayer
     
     #Their Step
   end
-
   
+  def max_of_nodes(nodes)
+    values = nodes.map do |nodes|
+      [nodes.first, self.min_of_last_nodes(nodes.last)]
+    end
+    max = values.max
+    values[values.index(max)].first
+  end
+
+  def min_of_last_nodes(nodes)
+    p nodes.last
+    values = nodes.map do |node|
+      node.last
+    end    #
+    # p values
+    values.min
+  end
   
   def valid_steps(board, player)
     #Return an array of all possible valid from  -> to combos 
@@ -104,16 +144,19 @@ if __FILE__ == $PROGRAM_NAME
   board = Board.new(true)
   King.new(board, [0,2], :white)
   King.new(board, [0,0], :black)
-  Rook.new(board, [2,2], :white)
+  Rook.new(board, [2,2], :white)  #
+  # Knight.new(board, [2,1], :black)
   player = ComputerPlayer.new(:white)
   board.render(:white)
-  start_time = Time.new
-  future_sight = player.future_tree(board, 2)  #
+  start_time = Time.new  #
+  # future_sight = player.future_tree(board, 1) #
   #puts future_sight.first.first #this is the move from, to combo  #
   # puts future_sight.first[1] # This is the first level state, and only  only the  state
   # p future_sight.first.last.first #this is the second level item, two moves forward
   end_time = Time.new
-  print "took #{end_time - start_time} seconds to generate future tree"
+  print "took #{end_time - start_time} seconds to generate future tree\n"
+  player.min_max(board)  #
+  # p future_sight.last[2]
   # Took 4.27 seconds with plain look ahead
   
   
